a69f6b8c8cccb9f1a14be0b3c3abdc6038c104d0
Mehdi <dr.m.mehdi.gholamrezaei97@gmail.com>
2023-10-07 21:53:39 +0330

    TEST4: production git hooks

--- public/pdfjs/web/app.js
+++ public/pdfjs/web/app.js
@@ -1,4 +1,4 @@
-//test git hooks3
+//test git hooks4
 const base_url = window.location.protocol + "//" + window.location.hostname;
 const backend_url = base_url + "/api/v1";
 const auth_page = base_url + "/admin/login";
