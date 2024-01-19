581d9e69b909c8a7c8e03ef05a1f560727adc176
Mrmmg <mrmmg76@gmail.com>
2024-01-19 17:45:52 +0330

    ADD: handle file delete on delete document

--- app/Filament/Resources/DocumentResource/Pages/EditDocument.php
+++ app/Filament/Resources/DocumentResource/Pages/EditDocument.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\DocumentResource\Pages;
 
 use App\Filament\Resources\DocumentResource;
 use Filament\Actions;
+use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Storage;
 
 class EditDocument extends EditRecord
 {
@@ -13,7 +16,13 @@ class EditDocument extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->modalDescription("Are you sure? The database record and file will be deleted!")
+                ->after(function (DeleteAction $action) {
+                    $record = $action->getRecord();
+
+                    Storage::disk('public')->delete($record->store_path);
+                }),
         ];
     }
 }
